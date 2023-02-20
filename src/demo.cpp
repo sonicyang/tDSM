d1a8f48ee1cffebba63ebe088ea17fdc66b1eaa5
Chung-Fan Yang <chungfan.yang@fixstars.com>
2023-02-20 23:42:51 +0900

    demo: disable the sem code for now

--- src/demo.cpp
+++ src/demo.cpp
@@ -21,30 +21,32 @@ int main() {
     auto& swapper = tDSM::swapper::get();
     auto memory = swapper.memory();
 
-    //std::uint8_t i = 0;
-    //while(true) {
-        //spdlog::info("{:x} {:x}", memory[0], memory[1]);
-        //memory[0] = i;
-        //i++;
-        //sleep(1);
-    //}
-
-    swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[0]));
-
-    bool stopped = false;
+    std::uint8_t i = 0;
     while(true) {
-        swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[0]));
-        spdlog::info("{}", memory[0]);
-        memory[0] = memory[0] + 1;
-        swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[0]));
-        sleep(1);
-        if (memory[0] == 20) {
-            swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[1]));
-        } else if (memory[0] > 40 && !stopped) {
-            stopped = true;
-            swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[1]));
-        }
+        spdlog::info("{:x} {:x}", memory[0], memory[1]);
+        memory[0] = i;
+        i++;
+        usleep(1000);
+        //assert(tDSM::packet::my_id != 0);
+
     }
+
+    //swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[0]));
+
+    //bool stopped = false;
+    //while(true) {
+        //swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[0]));
+        //spdlog::info("{}", memory[0]);
+        //memory[0] = memory[0] + 1;
+        //swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[0]));
+        //sleep(1);
+        //if (memory[0] == 20) {
+            //swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[1]));
+        //} else if (memory[0] > 40 && !stopped) {
+            //stopped = true;
+            //swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[1]));
+        //}
+    //}
 
     sleep(100000);
     return 0;
--- src/demo2.cpp
+++ src/demo2.cpp
@@ -21,26 +21,26 @@ int main() {
     auto& swapper = tDSM::swapper::get();
     auto memory = swapper.memory();
 
-    //std::uint8_t i = 0;
-    //while(true) {
-        //spdlog::info("{:x} {:x}", memory[0], memory[1]);
-        //memory[1] = i;
-        //i++;
-        //sleep(1);
-    //}
-
-    swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[1]));
-
+    std::uint8_t i = 0;
     while(true) {
-        swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[0]));
-        spdlog::info("{}", memory[0]);
-        memory[0] = memory[0] + 1;
-        swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[0]));
-        sleep(1);
-        if (memory[0] == 80) {
-            swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[1]));
-        }
+        spdlog::info("{:x} {:x}", memory[0], memory[1]);
+        memory[1] = i;
+        i++;
+        usleep(1000);
     }
+
+    //swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[1]));
+
+    //while(true) {
+        //swapper.sem_get(reinterpret_cast<std::uintptr_t>(&memory[0]));
+        //spdlog::info("{}", memory[0]);
+        //memory[0] = memory[0] + 1;
+        //swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[0]));
+        //sleep(1);
+        //if (memory[0] == 80) {
+            //swapper.sem_put(reinterpret_cast<std::uintptr_t>(&memory[1]));
+        //}
+    //}
 
     sleep(5);
     return 0;
